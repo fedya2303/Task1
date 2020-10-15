b07848e9e2586f1e8a1314c2991165c69a5026b2
Fedya Potapchenko <potapchenkof@gmail.com>
2020-10-15 21:03:50 +0300

    ADD FEATURE

--- client/app/src/main/java/com/example/taskone/activities/MainActivity.java
+++ client/app/src/main/java/com/example/taskone/activities/MainActivity.java
@@ -9,14 +9,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.taskone.viewmodel.MyViewModel;
 import com.example.taskone.R;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MyViewModel viewModel = new MyViewModel();;
+    private MyViewModel viewModel = new MyViewModel();
+    ;
     EditText hostEditText;
     Button sendReqButton;
     EditText portEditText;
     TextView responseText;
+    private String mAppUnitId = "ca-app-pub-7526080045582593~7855797963";
+    private String mInterstitialAdUnitId = "ca-app-pub-3940256099942544/1033173712";
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +34,26 @@ public class MainActivity extends AppCompatActivity {
         portEditText = findViewById(R.id.port_edit_text);
         responseText = findViewById(R.id.grpc_response_text);
 
+        mInterstitialAd = new InterstitialAd(this);
+        initializeInterstitialAd(mAppUnitId);
+
+        loadInterstitialAd(mInterstitialAdUnitId);
+
         sendReqButton.setOnClickListener(view -> {
             viewModel.sendRequest(hostEditText.getText().toString(), portEditText.getText().toString());
         });
 
         viewModel.getNumber().observe(this,
                 number -> responseText.setText(number.toString()));
+    }
+    private void initializeInterstitialAd(String appUnitId) {
+
+        MobileAds.initialize(this, appUnitId);
+
+    }
+
+    private void loadInterstitialAd(String interstitialAdUnitId) {
+        mInterstitialAd.setAdUnitId(interstitialAdUnitId);
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
     }
 }
